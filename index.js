2d2c860ec629007bc6a67da7d971ed2c27fb1a27
+ <familymanjosh@gmail.com>
2023-02-22 19:50:13 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    const hq = 42
+    if (blocks>hq){
+        return blocks-hq
+    } else{
+        return hq-blocks
+    }
+}
+function distanceFromHqInFeet(blocks){
+    return distanceFromHqInBlocks(blocks)*264
+}
+function distanceTravelledInFeet(start,destination){
+if (destination>start){
+    return (destination-start)*264
+} else{
+    return (start-destination)*264
+}
+}
+function calculatesFarePrice(start,destination){
+    const tripDistance = distanceTravelledInFeet(start,destination)
+    if (tripDistance<400){
+        return 0
+    } else if(tripDistance<2000){
+        return (tripDistance-400)*.02
+    } else if(tripDistance<2500){
+        return 25
+    } else if(tripDistance>2500){
+        return 'cannot travel that far'
+    }
+}
